1cbd1a70294db9d07e803b3710e9eb4e91e32989
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-11-29 11:00:08 +0500

    apis download

--- src/server/api.js
+++ src/server/api.js
@@ -33,8 +33,58 @@ export default {
     project_update(data) {
         return server(`project/update`, "put", data)
     },
-    project_one(p = query){
+    project_one(p = query) {
         return server(`project/one_project?id=${p.id}`)
-    }
-
+    },
+    // category api
+    category_add(data) {
+        return server(`category/create`, "post", data);
+    },
+    category_items_add(data) {
+        return server(`category/create_items`, "post", data);
+    },
+    category_all() {
+        return server(`category/all`, "get")
+    },
+    category_update(data) {
+        return server(`category/update`, "put", data);
+    },
+    category_item_update(data) {
+        return server(`category/update_item`, "put", data);
+    },
+    category_one(p = query) {
+        return server(`category/one_category?id=${p.id}`)
+    },
+    category_item_one(p = query) {
+        return server(`category/one_category_item?id=${p.id}`)
+    },
+    // targets api
+    target_add(data) {
+        return server(`target/create`, "post", data);
+    },
+    target_all() {
+        return server(`target/all`, "get");
+    },
+    target_update(data) {
+        return server(`target/update`, "put", data);
+    },
+    target_update_count_watch(data) {
+        return server(`target/update_count_watches`, "put", data);
+    },
+    target_one(p = query) {
+        return server(`target/one_target?id=${p = id}`, "get");
+    },
+    // files api
+    file_add(data) {
+        return server(`file/add`, "post", data)
+    },
+    file_files_source(data) {
+        return server(`file/files_by_source_id`, "post", data)
+    },
+    file_files_id(data) {
+        return server(`file/files_by_id`, "post", data)
+    },
+    file_update_id(data) {
+        return server(`file/update_file_by_id`, "put", data)
+    },
 };
