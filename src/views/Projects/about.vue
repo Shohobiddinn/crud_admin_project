be9f3d5a7e3263b7fed21647f639d565c16b06a0
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-12-25 17:44:51 +0500

    html edit

--- src/views/Projects/about.vue
+++ src/views/Projects/about.vue
@@ -46,83 +46,83 @@
             </div>
           </label>
         </div>
-        <div class="title-box justify-content-between">
-          <div class="d-flex">
-            <img src="/src/assets/images/slash.svg" alt="" />
-            <h3
-              @click="about2()"
-              v-if="!about.ikki"
-              class="contact_title align-self-end"
-            >
-              {{ aboutTitleData[1]?.text }}
-            </h3>
-            <input
-              v-else
-              type="text"
-              :value="aboutTitleData[1]?.text"
-              @change="updateCategory($event, aboutTitleData[1])"
-            />
-          </div>
-          <div
-            class="btn btn-success"
-            @click="
-              ($refs.filePage.status = 'video'),
-                ($refs.filePage.sourceNumber = 12),
-                $refs.filePage.open()
-            "
+        <div class="title-box">
+          <img src="/src/assets/images/slash.svg" alt="" />
+          <h3
+            @click="about2()"
+            v-if="!about.ikki"
+            class="contact_title align-self-end"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="16"
-              width="14"
-              fill="#fff"
-              viewBox="0 0 448 512"
-            >
-              <path
-                d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
-              />
-            </svg>
-          </div>
+            {{ aboutTitleData[1]?.text }}
+          </h3>
+          <input
+            v-else
+            type="text"
+            :value="aboutTitleData[1]?.text"
+            @change="updateCategory($event, aboutTitleData[1])"
+          />
         </div>
         <div class="video_carusel">
-          <carousel :items-to-show="4" :breakpoints="breakPoints">
-            <slide
-              class="video_carusel_slide d-flex flex-column"
-              v-for="item in swiperVideoData"
-              :key="item.id"
+          <div>
+            <div
+              class="btn btn-success mb-3"
+              @click="
+                ($refs.filePage.status = 'video'),
+                  ($refs.filePage.sourceNumber = 12),
+                  $refs.filePage.open()
+              "
             >
-              <VideoPlayer
-                :src="url + item?.file"
-                :fullscreen="true"
-                :controls="true"
-                :responsive="true"
-                style="width: 100%"
-              />
-              <input
-                type="file"
-                :id="item.id"
-                class="d-none"
-                accept="video/*"
-                @change="updateFileSwiperVideo($event, item)"
-              />
-              <label :for="item.id">
-                <div
-                  class="card_content_btn text-bg-warning text-uppercase btn"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="18"
-                    width="18"
-                    viewBox="0 0 512 512"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="16"
+                width="14"
+                fill="#fff"
+                viewBox="0 0 448 512"
+              >
+                <path
+                  d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
+                />
+              </svg>
+            </div>
+            <carousel :items-to-show="4" :breakpoints="breakPoints">
+              <slide
+                class="video_carusel_slide d-flex flex-column"
+                v-for="item in swiperVideoData"
+                :key="item.id"
+              >
+                <VideoPlayer
+                  :src="url + item?.file"
+                  :fullscreen="true"
+                  :controls="true"
+                  :responsive="true"
+                  style="width: 100%"
+                />
+                <input
+                  type="file"
+                  :id="item.id"
+                  class="d-none"
+                  accept="video/*"
+                  @change="updateFileSwiperVideo($event, item)"
+                />
+                <label :for="item.id">
+                  <div
+                    class="card_content_btn text-bg-warning text-uppercase btn"
                   >
-                    <path
-                      d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
-                    />
-                  </svg>
-                </div>
-              </label>
-            </slide>
-          </carousel>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="18"
+                      width="18"
+                      viewBox="0 0 512 512"
+                    >
+                      <path
+                        d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
+                      />
+                    </svg>
+                  </div>
+                </label>
+              </slide>
+            </carousel>
+          </div>
         </div>
         <div class="title-box">
           <img src="/src/assets/images/slash.svg" alt="" />
@@ -137,9 +137,9 @@
           />
         </div>
         <div class="client-box">
-          <div class="mb-5 d-flex align-items-center juctify-content-between">
+          <div class="mb-5">
             <div
-              class="btn btn-success me-3"
+              class="btn btn-success mb-3"
               @click="
                 ($refs.filePage.status = 'image'),
                   ($refs.filePage.sourceNumber = 13),
@@ -157,7 +157,7 @@
                   d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
                 />
               </svg>
-            </div>  
+            </div>
             <swiper
               class="current_users_swiper"
               :slidesPerView="4"
@@ -216,9 +216,9 @@
               <div class="swiper-button-next"></div>
             </swiper>
           </div>
-          <div class="d-flex align-items-center juctify-content-between">
+          <div class="">
             <div
-              class="btn btn-success me-3"
+              class="btn btn-success mb-3"
               @click="
                 ($refs.filePage.status = 'image'),
                   ($refs.filePage.sourceNumber = 14),
--- src/views/Projects/videos.vue
+++ src/views/Projects/videos.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="background">
     <addFilePage @end="getVideoQoshimcha()" ref="filePage" />
-    
+
     <sidebar />
     <div class="videos">
       <div class="title-box">
@@ -57,9 +57,8 @@
         </label>
       </div>
 
-      <div class="title-box justify-content-between">
-        <div class="d-flex">
-          <img src="/src/assets/images/slash.svg" alt="" />
+      <div class="title-box ">
+        <img src="/src/assets/images/slash.svg" alt="" />
           <h4
             class="contact_title align-self-end"
             @click="videos3()"
@@ -73,43 +72,64 @@
             :value="videosTitleData[0]?.text"
             @change="categoryUpdate($event, videosTitleData[0])"
           />
-        </div>
-        <div class="btn btn-success"       @click="
-          ($refs.filePage.status = 'video'),
-            $refs.filePage.sourceNumber = 10,
-            $refs.filePage.open()
-        "><svg xmlns="http://www.w3.org/2000/svg" height="16" width="14"  fill="#fff" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg></div>
+    
       </div>
-      <div class="video_content d-flex justify-content-center">
-        <div class="content" v-for="item in videoQoshimchaData" :key="item.id">
-          <VideoPlayer
-            :src="url + item?.file"
-            :controls="true"
-            fullscreen="true"
-            responsive="true"
-            style="width: 100%"
-          />
-          <input
-            type="file"
-            accept="video/*"
-            @change="fileUpdate($event, item)"
-            class="d-none"
-            :id="item.id"
-          />
-          <label :for="item.id">
-            <div class="card_content_btn text-bg-warning text-uppercase btn">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="18"
-                width="18"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
-                />
-              </svg>
-            </div>
-          </label>
+      <div class="video_contents">
+        <div
+          class="btn btn-success mb-3"
+          @click="
+            ($refs.filePage.status = 'video'),
+              ($refs.filePage.sourceNumber = 10),
+              $refs.filePage.open()
+          "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="16"
+            width="14"
+            fill="#fff"
+            viewBox="0 0 448 512"
+          >
+            <path
+              d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
+            />
+          </svg>
+        </div>
+        <div class="video_content d-flex justify-content-center">
+          <div
+            class="content"
+            v-for="item in videoQoshimchaData"
+            :key="item.id"
+          >
+            <VideoPlayer
+              :src="url + item?.file"
+              :controls="true"
+              fullscreen="true"
+              responsive="true"
+              style="width: 100%"
+            />
+            <input
+              type="file"
+              accept="video/*"
+              @change="fileUpdate($event, item)"
+              class="d-none"
+              :id="item.id"
+            />
+            <label :for="item.id">
+              <div class="card_content_btn text-bg-warning text-uppercase btn">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="18"
+                  width="18"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
+                  />
+                </svg>
+              </div>
+            </label>
+          </div>
         </div>
       </div>
       <contactBox />
@@ -129,7 +149,7 @@ export default {
     VideoPlayer,
     contactBox,
     sidebar,
-    addFilePage
+    addFilePage,
   },
   data() {
     return {
